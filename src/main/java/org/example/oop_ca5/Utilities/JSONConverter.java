523005e88b52013e4358f900fbe28775ad9ebc7d
harrist03 <harristeh85@gmail.com>
2025-04-05 14:01:55 +0100

    dependencies added for json conversion + utility class created

---
+++ src/main/java/org/example/oop_ca5/Utilities/JSONConverter.java
@@ -0,0 +1,12 @@
+package org.example.oop_ca5.Utilities;
+
+import org.example.oop_ca5.DTOs.Car;
+import org.example.oop_ca5.Exceptions.DaoException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.List;
+
+
+public class JSONConverter {
+    
+}
