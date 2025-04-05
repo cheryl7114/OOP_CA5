8c9378c2cad3b2307b4b1780b9f17c72b67aa174
harrist03 <harristeh85@gmail.com>
2025-04-05 14:09:19 +0100

    method to convert list of objects to JSON string completed

--- src/main/java/org/example/oop_ca5/Utilities/JSONConverter.java
+++ src/main/java/org/example/oop_ca5/Utilities/JSONConverter.java
@@ -8,5 +8,27 @@ import java.util.List;
 
 
 public class JSONConverter {
-    
+    // Convert list of car objects to JSON string
+    public static String carListToJSONString(List<Car> carList) throws DaoException {
+        try {
+            JSONArray jsonArray = new JSONArray();
+            
+            for (Car car : carList) {
+                // convert each car to json object
+                JSONObject carJSONObject = new JSONObject();
+                carJSONObject.put("carId", car.getCarId());
+                carJSONObject.put("make", car.getMake());
+                carJSONObject.put("model", car.getModel());
+                carJSONObject.put("year", car.getYear());
+                carJSONObject.put("rentalPricePerDay", car.getRentalPricePerDay());
+                carJSONObject.put("availability", car.isAvailable());
+
+                jsonArray.put(carJSONObject);
+            }
+            // return as a json string
+            return jsonArray.toString(4);
+        } catch (Exception e) {
+            throw new DaoException("Error converting car list to JSON");
+        }
+    }
 }
