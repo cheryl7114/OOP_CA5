58653e045cc6b5302bcf2423ff6350128444b5c6
harrist03 <harristeh85@gmail.com>
2025-04-05 14:39:20 +0100

    method to convert car object to json string completed

--- src/main/java/org/example/oop_ca5/Utilities/JSONConverter.java
+++ src/main/java/org/example/oop_ca5/Utilities/JSONConverter.java
@@ -8,27 +8,44 @@ import java.util.List;
 
 
 public class JSONConverter {
-    // Convert list of car objects to JSON string
+    // Feature 7: Convert list of car objects to JSON string
     public static String carListToJSONString(List<Car> carList) throws DaoException {
         try {
             JSONArray jsonArray = new JSONArray();
             
             for (Car car : carList) {
                 // convert each car to json object
-                JSONObject carJSONObject = new JSONObject();
-                carJSONObject.put("carId", car.getCarId());
-                carJSONObject.put("make", car.getMake());
-                carJSONObject.put("model", car.getModel());
-                carJSONObject.put("year", car.getYear());
-                carJSONObject.put("rentalPricePerDay", car.getRentalPricePerDay());
-                carJSONObject.put("availability", car.isAvailable());
-
+                JSONObject carJSONObject = carToJSONObject(car); 
                 jsonArray.put(carJSONObject);
             }
             // return as a json string
             return jsonArray.toString(4);
         } catch (Exception e) {
-            throw new DaoException("Error converting car list to JSON");
+            throw new DaoException("Error converting car list to JSON " + e.getMessage());
         }
+    }
+
+    // Feature 8: Convert single car object to JSON string
+    public static String carObjectToJSONString(Car car) throws DaoException {
+        try {
+            JSONObject carJSONObject = carToJSONObject(car);
+
+            return carJSONObject.toString(4);
+        } catch (Exception e) {
+            throw new DaoException("Error converting car to JSON " + e.getMessage());
+        }
+    } 
+
+    // method to convert car object to json object
+    private static JSONObject carToJSONObject(Car car) {
+        JSONObject carJson = new JSONObject();
+        carJson.put("carID", car.getCarId());
+        carJson.put("make", car.getMake());
+        carJson.put("model", car.getModel());
+        carJson.put("year", car.getYear());
+        carJson.put("rentalPricePerDay", car.getRentalPricePerDay());
+        carJson.put("availability", car.isAvailable());
+        
+        return carJson;
     }
 }
