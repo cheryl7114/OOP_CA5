6927f46dbd4394196523ec02c4045004a5107268
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-04-18 18:39:25 +0100

    test FindCarById

--- src/test/java/org/example/oop_ca5/DAOs/MySqlCarDaoTest.java
+++ src/test/java/org/example/oop_ca5/DAOs/MySqlCarDaoTest.java
@@ -5,6 +5,8 @@ import org.example.oop_ca5.Exceptions.DaoException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MySqlCarDaoTest {
@@ -13,5 +15,17 @@ class MySqlCarDaoTest {
     @BeforeEach
     void setUp() {
         carDao = new MySqlCarDao();  // create the DAO class
+    }
+
+    @Test
+    void testFindCarById() throws DaoException {
+        // find car with id 1
+        Car car = carDao.findCarById(1);
+        assertNotNull(car);    // check that it did not return null
+        assertEquals(1, car.getCarId());  // check that carId matches
+        assertNotNull(car.getMake());
+        assertNotNull(car.getModel());
+        assertTrue(car.getYear() > 0);
+        assertTrue(car.getRentalPricePerDay() > 0);
     }
 }
