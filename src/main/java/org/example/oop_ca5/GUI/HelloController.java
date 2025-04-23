f3eae0d10240b3da62c2c2e14eaf3c321679dd49
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-04-23 18:53:32 +0100

    setup simple menu

--- src/main/java/org/example/oop_ca5/GUI/HelloController.java
+++ src/main/java/org/example/oop_ca5/GUI/HelloController.java
@@ -1,14 +1,31 @@
 package org.example.oop_ca5.GUI;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void handleFindCarById() {
+        showAlert("Find Car by ID", "This feature will be implemented soon.");
+    }
+
+    @FXML
+    protected void handleDisplayAllCars() {
+        showAlert("Display All Cars", "This feature will be implemented soon.");
+    }
+
+    @FXML
+    protected void handleExit() {
+        System.exit(0);
+    }
+
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 }
